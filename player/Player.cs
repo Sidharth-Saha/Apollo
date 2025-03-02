dad18b7defa5667d1e01a384709fc34a69b67f78
Sidharth Saha <ssaha2000@gmail.com>
2025-03-02 14:36:39 -0700

    Implement player movement.

--- player/Player.cs
+++ player/Player.cs
@@ -3,6 +3,48 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
+	[Export]
+	public float Speed = 50.0f;
+
+	private Vector2[] validMovementDirections =
+	{
+		Vector2.Up,
+		Vector2.Right,
+		Vector2.Down,
+		Vector2.Left,
+		(Vector2.Up + Vector2.Right).Normalized(),
+		(Vector2.Up + Vector2.Left).Normalized(),
+		(Vector2.Down + Vector2.Right).Normalized(),
+		(Vector2.Down + Vector2.Left).Normalized()
+	};
+
+	private void ProcessMovementInput()
+	{
+		Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
+		// Pick the valid movement direction that is closest to the input direction
+		Vector2 movementDirection = Vector2.Zero;
+		if (inputDirection.LengthSquared() > 0.0f)
+		{
+			float maxDotProduct = 0.0f;
+			foreach (Vector2 direction in validMovementDirections)
+			{
+				float dotProduct = inputDirection.Dot(direction);
+				if (dotProduct > maxDotProduct)
+				{
+					maxDotProduct = dotProduct;
+					movementDirection = direction;
+				}
+			}
+		}
+		Velocity = movementDirection * Speed;
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+		ProcessMovementInput();
+		MoveAndSlide();
+    }
+    /*
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
 
@@ -37,4 +79,5 @@ public partial class Player : CharacterBody2D
 		Velocity = velocity;
 		MoveAndSlide();
 	}
+	*/
 }
